fa43c536343c96f977129d978c501c8690deb6db
John Jeremy <johnjeremyestrada@gmail.com>
2026-08-30 12:00:32 +0800

    add features

---
+++ demokotlin/src/main/kotlin/com/sample/demokotlin/DemokotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.sample.demokotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemokotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemokotlinApplication>(*args)
+}
