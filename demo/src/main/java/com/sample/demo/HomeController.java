ae7e7954c8915e99cb4c4b4d597503e7563a5d6a
John Jeremy <johnjeremyestrada@gmail.com>
2026-08-30 10:58:06 +0800

    add

--- demo/src/main/java/com/sample/demo/HomeController.java
+++ demo/src/main/java/com/sample/demo/HomeController.java
@@ -1,13 +1,39 @@
 package com.sample.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+class Solution {
+    private String name;
+    private String lname;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+}
 
 @RestController
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello World";
+        Solution s = new Solution();
+        s.setName("John");
+        s.setLname("Doe wcu");
+        return s.getName() + " " + s.getLname();
     }
 }
